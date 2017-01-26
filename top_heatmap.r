6458075315673d70c2372162fb4d70fc34280f94
Türei Dénes <turei.denes@gmail.com>
2017-01-26 16:05:42 +0000

    heatmap with effect signs in R

--- top_heatmap.r
+++ top_heatmap.r
@@ -35,12 +35,18 @@ p <- ggplot(fctop.m, aes(variable, label)) +
 
 ggsave('gg_fcdiff_top_heatmap.pdf', device = cairo_pdf, width = 4, height = 48)
 
+### with effects:
+
+
+
+###
+
 # Only top 50:
 
 fctop50 <- head(fctop, 111)
 rnames <- fctop50$psite
 rownames(fctop50) <- rnames
-mfctop50 <- as.matrix(fctop50[,6:8])
+mfctop50 <- as.matrix(fctop50[,9:11])
 rownames(mfctop50) <- rnames
 psites_dendro <- as.dendrogram(hclust(dist(mfctop50)))
 ordr <- order.dendrogram(psites_dendro)
@@ -74,7 +80,7 @@ ggsave('gg_fcdiff_top50_heatmap.pdf', device = cairo_pdf, width = 6, height = 14
 headfctop <- head(fctop, 100)
 rnames <- headfctop$label
 rownames(headfctop) <- rnames
-mheadfctop <- as.matrix(headfctop[,8:10])
+mheadfctop <- as.matrix(headfctop[,9:11])
 rownames(mheadfctop) <- rnames
 psites_dendro <- as.dendrogram(hclust(dist(mheadfctop / apply(abs(mheadfctop), 1, max))))
 ordr <- order.dendrogram(psites_dendro)
@@ -86,6 +92,25 @@ dfheadfctop$psite <- onames[[1]]
 dfheadfctop$psite <- with(dfheadfctop, factor(psite, levels = psite, ordered = TRUE))
 mltdfheadfctop <- melt(dfheadfctop, id.vars = 'psite')
 dendrodatapsite <- dendro_data(psites_dendro)
+
+signedfctop <- fctop[fctop$effect != 0,]
+signedfctop[,9:11] <-  signedfctop[,9:11] * signedfctop$effect
+headfctop <- head(signedfctop, 100)
+rnames <- headfctop$label
+rownames(headfctop) <- rnames
+mheadfctop <- as.matrix(headfctop[,9:11])
+rownames(mheadfctop) <- rnames
+psites_dendro <- as.dendrogram(hclust(dist(mheadfctop / apply(abs(mheadfctop), 1, max))))
+ordr <- order.dendrogram(psites_dendro)
+oheadfctop <- mheadfctop[ordr,]
+onames <- attr(oheadfctop, 'dimnames')
+dfheadfctop <- as.data.frame(oheadfctop)
+colnames(dfheadfctop) <- onames[[2]]
+dfheadfctop$psite <- onames[[1]]
+dfheadfctop$psite <- with(dfheadfctop, factor(psite, levels = psite, ordered = TRUE))
+mltdfheadfctop <- melt(dfheadfctop, id.vars = 'psite')
+dendrodatapsite <- dendro_data(psites_dendro)
+
 
 # Set up a blank theme
 theme_none <- theme(
@@ -124,8 +149,15 @@ p3 <- ggplot(segment(dendrodatapsite)) +
     coord_flip(xlim = c(0, 100)) +
     theme_none
 
-cairo_pdf(filename = 'gg_heatmap_dendro.pdf', width = 8, height = 14)
+cairo_pdf(filename = 'gg_heatmap_dendro_signed.pdf', width = 8, height = 14)
     grid.newpage()
     print(p1, vp=viewport(0.8, 1.0, x=.4, y=.5))
     print(p3, vp=viewport(0.2, 1.076, x=0.88, y=0.498))
 dev.off()
+
+cairo_pdf(filename = 'gg_heatmap_dendro.pdf', width = 8, height = 14)
+grid.newpage()
+print(p1, vp=viewport(0.8, 1.0, x=.4, y=.5))
+print(p3, vp=viewport(0.2, 1.076, x=0.88, y=0.498))
+dev.off()
+
